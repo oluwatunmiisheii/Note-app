d2e4405075c8f59d4d663f78ea81468e138ce059
Adenuga oluwatunmise <oluwatunmiseadenuga@gmail.com>
2023-02-14 18:38:01 +0100

    reate reuseable input component

---
+++ components/Form/Input/Input.tsx
@@ -0,0 +1,62 @@
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TextInputProps,
+  ViewStyle,
+  StyleProp,
+  Text,
+} from "react-native";
+import colors from "../../../utils/constants/colors";
+import { InputError } from "../InputError/InputError";
+
+interface InputProps extends TextInputProps {
+  extraStyles?: StyleProp<ViewStyle>;
+  label: string;
+  error?: string;
+}
+
+export const Input = (props: InputProps) => {
+  const inputStyles = [styles.input, props.multiline && styles.multiline];
+
+  return (
+    <View style={{ marginBottom: 20 }}>
+      <Text style={styles.label}>{props.label}</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={[inputStyles, props.extraStyles]}
+          placeholderTextColor="#BDBDBD"
+          {...props}
+        />
+      </View>
+      {props.error && <InputError error={props.error} />}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F5F5F5",
+    width: "100%",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    justifyContent: "space-between",
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "500",
+    marginBottom: 5,
+    color: colors.gray,
+  },
+  input: {
+    height: 50,
+    fontSize: 18,
+    fontWeight: "700",
+    paddingHorizontal: 2,
+    textAlignVertical: "top",
+  },
+  multiline: {
+    height: 250,
+  },
+});
