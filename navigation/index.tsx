ce46cf1b9f386271dbed3264da6555142c735aa1
Adenuga oluwatunmise <oluwatunmiseadenuga@gmail.com>
2023-02-14 18:44:07 +0100

    add note details screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,17 +1,27 @@
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { ColorSchemeName } from "react-native";
 
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import HomeScreen from "../screens/HomeScreen";
+import NoteDetails from "../screens/NoteDetailsScreen";
 
-import HomeScreen from '../screens/HomeScreen';
+import { RootStackParamList } from "../types";
+import colors from "../utils/constants/colors";
 
-import { RootStackParamList } from '../types';
-
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
     <NavigationContainer
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+    >
       <RootNavigator />
     </NavigationContainer>
   );
@@ -21,8 +31,29 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Notes' }} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: "Notes",
+        }}
+      />
+      <Stack.Screen
+        name="NoteDetailsScreen"
+        component={NoteDetails}
+        options={{ title: "Note" }}
+      />
     </Stack.Navigator>
   );
 }
