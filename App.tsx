9fa5f23bc17bd3c08c80fd19b8e2c1d4b2008bc9
Adenuga oluwatunmise <oluwatunmiseadenuga@gmail.com>
2023-02-14 18:47:45 +0100

    add styling for status bar on ios

--- App.tsx
+++ App.tsx
@@ -1,25 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { Platform, View, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Providers } from "./context/Providers/Providers";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import colors from "./utils/constants/colors";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const APPBAR_HEIGHT = isLoadingComplete && Platform.OS === "ios" ? 60 : 0;
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <SafeAreaView style={{flex: 1}}>
-
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-        </SafeAreaView>
+        <Providers>
+          <SafeAreaView style={{ flex: 1 }}>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar backgroundColor={colors.primary} />
+            <View style={[styles.appBar, { height: APPBAR_HEIGHT }]} />
+          </SafeAreaView>
+        </Providers>
       </SafeAreaProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  appBar: {
+    backgroundColor: colors.primary,
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    zIndex: -1,
+  },
+});
